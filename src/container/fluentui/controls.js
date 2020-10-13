ba23801e654c39674fba59a1781b942a631b1eb7
unknown <etc@dewise.com>
2020-10-13 16:41:12 +0800

    first commit

---
+++ src/container/fluentui/controls.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react'
+import { DefaultButton, PrimaryButton } from 'office-ui-fabric-react';
+
+class Controls extends Component {
+
+    render(){
+        return(<div>
+            <div>My Fluent UI</div>
+            <div>Button</div>
+            <div>
+            <DefaultButton text="Standard"  />
+            <PrimaryButton text="Primary"/>
+
+            </div>
+            <div class="ms-Grid" dir="ltr">
+            <div class="ms-Grid-row">
+                <div class="ms-Grid-col ms-sm6 ms-md4 ms-lg2">A</div>
+                <div class="ms-Grid-col ms-sm6 ms-md8 ms-lg10">B</div>
+            </div>
+            </div>
+
+        </div>);
+    }
+
+}
+
+export default Controls;
